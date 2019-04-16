02840d2c171bc035b4bb32a1b66a6a6a5c36fb3a
Pedro Pais <pg.pais@campus.fct.unl.pt>
2019-04-16 11:42:15 +0100

    Server's done(?) - Probably needs java implementation

---
+++ SD2019-Microgram-Code/src/microgram/impl/srv/soap/ProfilesWebService.java
@@ -0,0 +1,50 @@
+package microgram.impl.srv.soap;
+
+
+import java.util.List;
+
+import microgram.api.Profile;
+import microgram.api.java.Profiles;
+import microgram.api.soap.MicrogramException;
+import microgram.api.soap.SoapProfiles;
+import microgram.impl.srv.java.JavaProfiles;
+
+//Make this class concrete.
+public class ProfilesWebService extends SoapService implements SoapProfiles {
+
+	final Profiles impl;
+	
+	protected ProfilesWebService() {
+		this.impl = new JavaProfiles();
+	}
+	
+	@Override
+	public Profile getProfile( String userId ) throws MicrogramException {
+		return super.resultOrThrow( impl.getProfile(userId));
+	}
+
+	@Override
+	public void createProfile(Profile profile) throws MicrogramException {
+		super.resultOrThrow(impl.createProfile(profile));
+	}
+
+	@Override
+	public void deleteProfile(String userId) throws MicrogramException {
+		super.resultOrThrow(impl.deleteProfile(userId));
+	}
+
+	@Override
+	public List<Profile> search(String prefix) throws MicrogramException {
+		return super.resultOrThrow(impl.search(prefix));
+	}
+
+	@Override
+	public void follow(String userId1, String userId2, boolean isFollowing) throws MicrogramException {
+		super.resultOrThrow(impl.follow(userId1, userId2, isFollowing));
+	}
+
+	@Override
+	public boolean isFollowing(String userId1, String userId2) throws MicrogramException {
+		return super.resultOrThrow(impl.isFollowing(userId1, userId2));
+	}
+}
