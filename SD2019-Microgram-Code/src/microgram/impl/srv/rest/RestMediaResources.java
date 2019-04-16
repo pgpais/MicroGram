f1a621dc20ba3add634743a198e64c2b8496d3e8
Pedro Pais <pg.pais@campus.fct.unl.pt>
2019-04-16 14:39:19 +0100

    Added mediaServer (?)

---
+++ SD2019-Microgram-Code/src/microgram/impl/srv/rest/RestMediaResources.java
@@ -0,0 +1,37 @@
+package microgram.impl.srv.rest;
+
+import javax.ws.rs.WebApplicationException;
+
+import microgram.impl.srv.java.JavaMedia;
+import microgram.api.java.Media;
+import microgram.api.java.Result;
+import microgram.api.rest.RestMediaStorage;
+
+public class RestMediaResources extends RestResource implements RestMediaStorage {
+	
+	final Media impl;
+	final String baseUri;
+	
+	public RestMediaResources(String baseUri ) {
+		this.baseUri = baseUri + RestMediaStorage.PATH;
+		this.impl = new JavaMedia();
+	}
+	
+	@Override
+	public String upload(byte[] bytes) {
+		Result<String> result = impl.upload(bytes);
+		if (result.isOK())
+			return baseUri + "/" + result.value();
+		else
+			throw new WebApplicationException(super.statusCode(result));
+	}
+
+	@Override
+	public byte[] download(String id) {
+		Result<byte[]> result = impl.download( id );
+		if( result.isOK() )
+			return result.value();
+		else
+			throw new WebApplicationException( super.statusCode( result )) ;
+ 	}
+}
