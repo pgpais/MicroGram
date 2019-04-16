612067973a634ca3b0cf0d314c8c6700ea0e8a2c
Pedro Pais <pg.pais@campus.fct.unl.pt>
2019-04-16 11:50:49 +0100

    deletePost done - getFeed needs Profile?

--- SD2019-Microgram-Code/src/microgram/impl/srv/java/JavaPosts.java
+++ SD2019-Microgram-Code/src/microgram/impl/srv/java/JavaPosts.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import java.util.Set;
 
 import microgram.api.Post;
+import microgram.api.Profile;
 import microgram.api.java.Posts;
 import microgram.api.java.Result;
 import microgram.api.java.Result.ErrorCode;
@@ -37,7 +38,11 @@ public class JavaPosts implements Posts {
 
 	@Override
 	public Result<Void> deletePost(String postId) {
-		return Result.error(ErrorCode.NOT_IMPLEMENTED);
+		Post res = posts.remove(postId);
+		if (res != null)
+			return error(NOT_FOUND);
+		else
+			return ok();
 	}
 
 	@Override
@@ -96,6 +101,7 @@ public class JavaPosts implements Posts {
 
 	@Override
 	public Result<List<String>> getFeed(String userId) {
+		// TODO: Needs Profile?
 		return error(NOT_IMPLEMENTED);
 	}
 }
