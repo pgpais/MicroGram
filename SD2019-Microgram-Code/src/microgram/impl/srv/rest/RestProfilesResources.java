4ca32ebb71f9747da3ce3e71611ca5f80af7c7d2
cheeseskyy <pg.pais@campus.fct.unl.pt>
2019-05-01 14:31:29 +0100

    added getFollowing to implementation

--- SD2019-Microgram-Code/src/microgram/impl/srv/rest/RestProfilesResources.java
+++ SD2019-Microgram-Code/src/microgram/impl/srv/rest/RestProfilesResources.java
@@ -3,6 +3,7 @@ package microgram.impl.srv.rest;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Set;
 
 import microgram.api.Profile;
 import microgram.api.java.Profiles;
@@ -41,5 +42,10 @@ public class RestProfilesResources extends RestResource implements RestProfiles 
 	@Override
 	public boolean isFollowing(String userId1, String userId2) {
 		return super.resultOrThrow(impl.isFollowing(userId1, userId2));
+	}
+
+	@Override
+	public Set<String> getFollowing(String userId) {
+		return super.resultOrThrow(impl.getFollowing(userId));
 	}	
 }
