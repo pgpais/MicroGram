1fe61b9506d78ffb9f897ffd4c4fdc5bfa6f2e99
cheeseskyy <pg.pais@campus.fct.unl.pt>
2019-05-01 15:03:15 +0100

    changed port in profile server

--- SD2019-Microgram-Code/src/microgram/impl/srv/rest/ProfilesRestServer.java
+++ SD2019-Microgram-Code/src/microgram/impl/srv/rest/ProfilesRestServer.java
@@ -19,7 +19,7 @@ public class ProfilesRestServer {
 		System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s");
 	}
 	
-	public static final int PORT = 7777;
+	public static final int PORT = 7778;
 	public static final String SERVICE = "Microgram-Profiles";
 	public static String SERVER_BASE_URI = "http://%s:%s/rest";
 	
